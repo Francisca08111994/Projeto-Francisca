48638b73e4af16ad2a949325f737409bf9c4580f
Francisca08111994 <spintofrancisca@gmail.com>
2026-04-25 17:19:07 +0100

    Criar programa números iguais/diferentes

---
+++ numeros_iguais_diferentes.py
@@ -0,0 +1,16 @@
+num1 = int(input("Introduz número 1:"))
+num2 = int(input("Introduz número 2:"))
+print()
+
+#verifica se dois numeros são iguais 
+if num1 == num2:
+    print("Os números introduzidos são iguais")
+
+#Caso os numeros sejam diferentes
+else:
+    print ("Os números introduzidos são diferentes")
+    
+    
+
+
+
