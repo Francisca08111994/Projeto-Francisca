df87cf83f38db631ecc1b74ec2761c310b137336
Francisca <spintofrancisca@gmail.com>
2026-04-25 16:30:33 +0100

    Adicionar mensagem para números diferentes

---
+++ numeros_iguais_diferentes.py
@@ -0,0 +1,13 @@
+num1 = int(input("Introduz número 1:"))
+num2 = int(input("Introduz número 2:"))
+print()
+if num1 == num2:
+    print("Os números introduzidos são iguais")
+
+else:
+    print ("Os números introduzidos são diferentes")
+    
+    
+
+
+
